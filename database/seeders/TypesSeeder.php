c68d3d0b36ab055a12e83e183133b528950ee609
Iunora93 <eleonoraruggieri22@gmail.com>
2023-01-30 16:54:38 +0100

    Definisce la relazione one to many

---
+++ database/seeders/TypesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Types;
+use Mockery\Matcher\Type;
+use Illuminate\Support\Str;
+
+class TypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Types::truncate();
+        
+        $types = ['Frontend', 'Backend', 'Devops', 'AI'];
+
+        foreach( $types as $type ) {
+            $new_type = New Types();
+            $new_type->name = $type;
+            $new_type->slug = Str::slug($new_type->name);
+
+            $new_type->save();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\Admin\TypesController;
 use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -24,6 +25,8 @@ Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
     })->name('dashboard');
 
     Route::resource('projects', ProjectController::class)->parameters(['projects' => 'project:slug']);
+    Route::resource('types', TypesController::class)->parameters(['types' => 'type:slug']);
+
 });
 
 require __DIR__.'/auth.php';
